06494c6255ec6d7a6707698971782481081c9b1b
xiaomenggu <prairieyouheart@gmail.com>
2020-05-27 16:18:52 +0800

    vue3.0 first push

---
+++ src/components/common/tabbar/TabBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="page"></div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+name: 'TabBar',
+ data() {
+ return {
+
+ }
+ },
+ components: {
+
+ },
+beforeRouteEnter (to, from, next) {
+if (to.meta.title) {
+document.title = to.meta.title
+}
+next()
+},
+beforeRouteLeave (to, from, next) {
+next()
+},
+created () {
+},
+methods: {
+}
+}
+</script>
+
+<style scoped lang="less"></style>
